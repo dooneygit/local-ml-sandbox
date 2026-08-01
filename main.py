fc907df978471c46ab4511a110a759e471a29bb4
DooneyGit <dooneytrue@gmail.com>
2026-08-01 12:00:00 -0500

    feat(experiment): run linear regression example

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import keras
 import plotly.express as px
+from experiment import Hyperparameters, Experiment
+from model import create_model, train_model
+from plot import plot_experiment_metrics, plot_model_predictions
+
 
 taxi_dataset = pd.read_csv("taxi_train.csv")
 
@@ -23,5 +27,18 @@ pair_plot = px.scatter_matrix(
 
 pair_plot.show()
 
+hyperparameters_1 = Hyperparameters(
+    input_features=["TRIP_MILES"],
+    learning_rate=0.001,
+    batch_size=50,
+    number_epochs=20
+)
 
+metrics=[keras.metrics.RootMeanSquaredError(name='rmse')]
 
+model_1=create_model(hyperparameters_1, metrics)
+
+experiment_1 = train_model('one_feature_test', model_1, training_df, 'FARE', hyperparameters_1)
+
+plot_experiment_metrics(experiment_1, ["rmse"])
+plot_model_predictions(experiment_1, training_df, "FARE")
